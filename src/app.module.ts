1040aff57f4a6311b0dbb67a08f0cf01a4b4e385
sesh10 <sesha1011@gmail.com>
2024-12-26 19:49:02 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [MovieModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
+})
+export class AppModule {}
